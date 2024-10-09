edf520d64775998bf4b115eb32087a377e567f8b
Vlaad1955 <wylf1312@gmail.com>
2024-10-10 00:33:30 +0300

    hw2.1

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -3,7 +3,9 @@ import {axiosInstance} from "./user.service";
 import {PostModel} from "../models/PostModel";
 export const getPostsOfUser = async (user: UserModel) => {
     let response = await axiosInstance.get<{posts:PostModel[]}>('/posts', {
-        params: {userId: user.id}
+        params: {limit:0}
     });
-    return response.data.posts;
+    const filteredPosts = response.data.posts.filter(post => post.userId === user.id);
+    return filteredPosts;
 }
+
